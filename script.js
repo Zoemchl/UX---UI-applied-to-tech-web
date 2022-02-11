76d6f2580b3ee1783f1f4d3ee6cf55d859194f87
Zoé <zoe.michel@edu.devinci.fr>
2022-02-11 17:41:45 +0100

    :white_check_mark:code editor finish

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-console.log("bla");
-var editor = CodeMirror.fromTextArea
-(document.getElementById('editor'), {
-    mode: "xml",
-    theme: "dracula",
+console.log("test")
+
+const htmlEditor = CodeMirror(document.querySelector(".html_code"), {
     lineNumbers: true,
-    autoCloseTags: true
+    tabSize: 2,
+    mode:"xml"
 });
+
+const cssEditor = CodeMirror(document.querySelector(".css_code"), {
+    lineNumbers: true,
+    tabSize: 2,
+    mode:"css"
+});
+
+document.getElementById("run_btn").addEventListener("click", function(){
+    let htmlCode = htmlEditor.getValue();
+    let cssCode = "<style>" + cssEditor.getValue() + "<style>";
+    let previewWindow = document.querySelector("#preview_window").contentWindow.document;
+    previewWindow.open();
+    previewWindow.write(htmlCode+cssCode);
+    previewWindow.close();
+})
