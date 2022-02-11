288dc8d02f93bccf928e834eb5212ee63c6fe860
matisdn <matis.dene@edu.devinci.fr>
2022-02-11 15:40:19 +0100

    :tada: Matis first push

---
+++ script.js
@@ -0,0 +1,51 @@
+var i = 0;
+var txt = 'Pour commencer quel est ton prenom?';
+var speed = 60;
+var cUser = document.getElementById('cUser')
+var userName = document.getElementById('name')
+var varName = ""
+var activated = true
+var activatedLastName = true
+
+function typeWriter() {
+    if (i < txt.length) {
+    document.getElementById("questionFirstName").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+    else if( i = txt.length){
+        cUser.style.animation = 'yo 0.5s ease forwards'
+    }
+}
+
+typeWriter()
+
+window.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' && activated === true) {
+        if(userName.value != ""){
+            namePrint()
+            varName = userName.value   
+            activated = false
+        }
+    }
+})
+
+function namePrint(){
+    document.getElementById("questionFirstName").innerHTML = ""
+    varName = userName.value
+    userName.value = ""
+    txt = `Ok super ${varName}, et ton nom alors ?`
+    i = 0
+    typeWriter()
+    
+}
+
+window.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' &&  activatedLastName === true) {
+        if(userName.value != ""){
+            varlastName = userName.value   
+            activatedLastName = false
+            console.log(varlastName)
+        }
+    }
+})
